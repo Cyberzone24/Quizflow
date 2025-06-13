e2555ee9ddff90cc9baabe91c5e201c6e61f04c9
ByJuix <julian@toast24.de>
2025-06-13 16:15:38 +0200

    für neue Installation vorbereitet

--- includes/core/config.php
+++ includes/core/config.php
@@ -11,4 +11,3 @@ const DB_USER = 'username';
 const DB_PASSWORD = 'password';
 const QUIZFLOW_DATA = __DIR__ . '/../questions.json';
 const QUIZFLOW_CODE = __DIR__ . '/../code.txt';
-const ADMIN_PASSWORD_HASH = '$2y$10$WBVCRD5GJfSVRfgr0nfo9ehE2x3VYfVJRuQR.59NgghvUcuip/o1G';
